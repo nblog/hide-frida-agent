7be48309c179984ac03b946feb0e185ce88d919d
共产主义接班人 <503407184@qq.com>
2023-07-19 16:58:42 +0800

    adapted for `frida-16.1.3`

--- injectjs.js
+++ injectjs.js
@@ -1,26 +1,62 @@
-﻿
 
 const ptrlength = Process.pointerSize;
 
-const frida_agent = "frida-agent.dll"
-const moudleInfos = Process.getModuleByName(frida_agent)
+const RtlGetCurrentPeb = new NativeFunction(
+    Module.getExportByName("ntdll.dll", "RtlGetCurrentPeb"),
+    "pointer",
+    []
+);
+
+// https://learn.microsoft.com/windows/win32/api/processthreadsapi/nf-processthreadsapi-getprocessid
+const GetProcessId = new NativeFunction(
+    Module.getExportByName("kernel32.dll", "GetProcessId"),
+    "uint32",
+    ["pointer"]
+);
+
+// https://learn.microsoft.com/windows/win32/api/processthreadsapi/nf-processthreadsapi-getcurrentprocess
+const GetCurrentProcess = new NativeFunction(
+    Module.getExportByName("kernel32.dll", "GetCurrentProcess"),
+    "pointer",
+    []
+);
+
+// https://learn.microsoft.com/windows-hardware/drivers/ddi/wdm/nf-wdm-rtlzeromemory
+const RtlZeroMemory = new NativeFunction(
+    Module.getExportByName("ntdll.dll", "RtlZeroMemory"),
+    "void",
+    ["pointer", "size_t"]
+);
+
+// https://learn.microsoft.com/windows/win32/api/memoryapi/nf-memoryapi-virtualprotect
+const VirtualProtect = new NativeFunction(
+    Module.getExportByName("kernel32.dll", "VirtualProtect"),
+    "int",
+    ["pointer", "size_t", "uint32", "pointer"]
+);
 
 
-// https://docs.microsoft.com/en-us/windows-hardware/drivers/ddi/wdm/nf-wdm-rtlzeromemory
-const RtlZeroMemory = new NativeFunction(Module.getExportByName("ntdll.dll", "RtlZeroMemory"), 
-    "void", 
-    ["pointer", "pointer"]);
+// https://github.com/frida/frida-core/blob/main/lib/agent/frida-agent.def
+const frida_agent = (function() {
+    let hModule = Module.getBaseAddress("frida-agent.dll");
+    // https://learn.microsoft.com/windows/win32/api/psapi/ns-psapi-moduleinfo
+    const ModuleInfoSize = 3 * ptrlength;
+    let ModuleInfoPtr = Memory.alloc(ModuleInfoSize);
+    // https://learn.microsoft.com/windows/win32/api/psapi/nf-psapi-getmoduleinformation
+    new NativeFunction(
+        Module.getExportByName("psapi.dll", "GetModuleInformation"),
+        "bool",
+        ["pointer", "pointer", "pointer", "uint32"]
+    )(GetCurrentProcess(), hModule, ModuleInfoPtr, ModuleInfoSize);
+    return {
+        base: ModuleInfoPtr.add(0 * ptrlength).readPointer(),
+        size: ModuleInfoPtr.add(1 * ptrlength).readU32(),
+        entry: ModuleInfoPtr.add(2 * ptrlength).readPointer(),
+    }
+})();
 
-// https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualprotect
-const VirtualProtect = new NativeFunction(Module.getExportByName("kernel32.dll", "VirtualProtect"), 
-    "bool", 
-    ["pointer", "pointer", "uint32", "pointer"]);
 
-const RtlGetCurrentPeb = new NativeFunction(Module.getExportByName("ntdll.dll", "RtlGetCurrentPeb"), 
-    "pointer", 
-    []);
-
-// https://docs.microsoft.com/en-us/windows-hardware/drivers/ddi/wdm/nf-wdm-removeentrylist
+// https://learn.microsoft.com/windows-hardware/drivers/ddi/wdm/nf-wdm-removeentrylist
 function RemoveEntryList(
     Entry
 ) {
@@ -54,13 +90,15 @@ function EnumEntryList(
         const FullDllName = pLdrDataEntry.add(9 * ptrlength);
         const BaseDllName = pLdrDataEntry.add(9 * ptrlength + (2 * ptrlength));
 
-        // console.log("\n\nbase: " + DllBase
-        //     + "\nsize: " + SizeOfImage
-        //     + "\nfull: " + FullDllName.add(ptrlength).readPointer().readUtf16String()
-        //     + "\nname: " + BaseDllName.add(ptrlength).readPointer().readUtf16String()
-        // );
+        /*
+        console.log(`\n` + 
+            `base: ${DllBase}\n` + 
+            `size: ${SizeOfImage}\n` + 
+            `full: ${FullDllName.add(ptrlength).readPointer().readUtf16String()}\n` + 
+            `name: ${BaseDllName.add(ptrlength).readPointer().readUtf16String()}\n`);
+        */
 
-        if (moudleInfos.base.equals(DllBase)) {
+        if (frida_agent.base.equals(DllBase)) {
 
             FullDllName.add(0).writeU16(0);
             FullDllName.add(2).writeU16(0);
@@ -71,7 +109,7 @@ function EnumEntryList(
             BaseDllName.add(ptrlength).writePointer(ptr(0));
 
             RemoveEntryList(OrderModuleHead)
-		}
+        }
 
         OrderModuleHead = OrderModuleHead.add(ptrlength).readPointer();
         
@@ -105,62 +143,50 @@ function HideInInitializationOrderLinks() {
 }
 
 function EraseHeaders() {
-    const page = Process.pageSize;
+    const page_size = Process.pageSize;
 
     const PAGE_READWRITE = 0x04;
 
     var lpflOldProtect = Memory.alloc(4);
-    VirtualProtect(moudleInfos.base, ptr(page), PAGE_READWRITE, lpflOldProtect);
-    RtlZeroMemory(moudleInfos.base, ptr(page));
-    VirtualProtect(moudleInfos.base, ptr(page), lpflOldProtect.readU32(), lpflOldProtect);
+    VirtualProtect(frida_agent.base, page_size, PAGE_READWRITE, lpflOldProtect);
+    RtlZeroMemory(frida_agent.base, page_size);
+    VirtualProtect(frida_agent.base, page_size, lpflOldProtect.readU32(), lpflOldProtect);
 }
 
 
 function ShieldQuery() {
-    // https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getprocessid
-    const GetProcessId = new NativeFunction(Module.getExportByName("kernel32.dll", "GetProcessId"), 
-    "uint32", 
-    ["pointer"]);
-
-    // https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getcurrentprocess
-    const GetCurrentProcess = new NativeFunction(Module.getExportByName("kernel32.dll", "GetCurrentProcess"), 
-    "pointer", 
-    []);
-
-    // https://docs.microsoft.com/en-us/windows-hardware/drivers/ddi/ntifs/nf-ntifs-ntqueryvirtualmemory
+    // https://learn.microsoft.com/windows-hardware/drivers/ddi/ntifs/nf-ntifs-ntqueryvirtualmemory
     const NtQueryVirtualMemoryPtr = Module.getExportByName("ntdll.dll", "NtQueryVirtualMemory");
 
     Interceptor.attach(NtQueryVirtualMemoryPtr, {
-    onEnter(args) {
+        onEnter(args) {
+            this.bSkip = false;
 
-        this.bSkip = false;
+            const ProcessHandle = args[0];
+            const BaseAddress = args[1];
+            const MemoryInformationClass = args[2].toInt32();
 
-        const ProcessHandle = args[0];
-        const BaseAddress = args[1];
-        const MemoryInformationClass = args[2].toInt32();
-        
-        if (GetCurrentProcess() == ProcessHandle 
-            || Process.id == GetProcessId(ProcessHandle)) {
-            console.log("check self.\n");
-            
-            if (BaseAddress >= moudleInfos.base 
-                && BaseAddress <= moudleInfos.base.add(moudleInfos.size)) {
-                console.log("check addr: " + BaseAddress
-                    + "in \"" + frida_agent + "\" module.\n");
+            const MemoryInformationLength = args[4];
+            this.MemoryInformation = args[3];
+            this.ReturnLength = args[5];
+
+            // detect if the current process is the target process
+            if (GetCurrentProcess() != ProcessHandle 
+            || Process.id != GetProcessId(ProcessHandle)) return;
+
+            // detect if the query is for the frida agent module
+            if (BaseAddress >= frida_agent.base 
+                && BaseAddress <= frida_agent.base.add(frida_agent.size)) {
+                console.log(`detecting ${BaseAddress} in "frida-agent" module.`);
                 this.bSkip = true;
             }
+        },
+        onLeave(retval) {
+            if (this.bSkip) {
+                const STATUS_ACCESS_DENIED = ptr(0xC0000022);
+                retval.replace(STATUS_ACCESS_DENIED);
+            }
         }
-
-        const MemoryInformationLength = args[4];
-        this.MemoryInformation = args[3];
-        this.ReturnLength = args[5];
-    },
-    onLeave(retval) {
-        if (this.bSkip) {
-            // STATUS_ACCESS_DENIED (0xC0000022)
-            retval.replace(ptr("0xC0000022"));
-        }
-    }
     });
 }
 
@@ -169,3 +195,5 @@ HideInMemoryOrderLinks();
 HideInInitializationOrderLinks();
 ShieldQuery();
 EraseHeaders();
+
+console.log(`!!! warn: exception thrown when uninstalling(exit) module !!!`);
